f5aaa755f45b3e6226d0b3db303bfcb8006002ff
Renan Martins <renanmartins1813@gmail.com>
2022-08-13 15:55:46 -0500

    refactor: PK_LIST_URL for better pagination

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const PK_LIST_URL = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=15`;
-export const SINGLE_PK_URL = (id)=> `https://pokeapi.co/api/v2/pokemon/${id}`;
+export const PK_LIST_URL = (current) => `https://pokeapi.co/api/v2/pokemon?offset=${current}&limit=15`;
+export const SINGLE_PK_URL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
