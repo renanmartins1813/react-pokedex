6c33531573a37f612422f0041feaa26152203b72
Renan Martins <renanmartins1813@gmail.com>
2022-08-12 23:32:05 -0500

    chore: update constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,2 @@
-export default {
-    POKEAPI_PK_LIST_URL: `https://pokeapi.co/api/v2/pokemon`,
-    POKEAPI_SINGLE_PK_URL: (id)=> `https://pokeapi.co/api/v2/pokemon/${id}`
-}
+export const PK_LIST_URL = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=15`;
+export const SINGLE_PK_URL = (id)=> `https://pokeapi.co/api/v2/pokemon/${id}`;
