2b15f3b2b85eb8b3b2eed4004e1a90a18e91f9de
Renan Martins <renanmartins1813@gmail.com>
2022-08-14 17:30:29 -0500

    feat: show pokemon status

--- src/common/components/Pokemon.jsx
+++ src/common/components/Pokemon.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import useFetch from '../../hooks/useFetch'
 import { SINGLE_PK_URL } from '../../utils/constants'
 
 export default function Pokemon({name}) {
-    const {data: pokemon, isLoading} = useFetch(SINGLE_PK_URL(name))
+    const {data: pokemon, isLoading} = useFetch(SINGLE_PK_URL(name));
+
+    useEffect(()=>{
+        console.log(pokemon);
+    }, [pokemon])
 
     return (
         <>
-            {isLoading ? 'loading' : <h1>{pokemon.name}</h1>}
+            {isLoading ? 'loading' : (
+                <>
+                    <h1>{pokemon.name}</h1>
+                    <img src={pokemon.sprites.other['official-artwork'].front_default} alt={`Pokemon ${pokemon.name}'s Official Artwork`} />
+                    <p>{pokemon.stats[0].stat.name} {pokemon.stats[0].base_stat}</p>
+                    <p>{pokemon.stats[1].stat.name} {pokemon.stats[1].base_stat}</p>
+                    <p>{pokemon.stats[2].stat.name} {pokemon.stats[2].base_stat}</p>
+                    <p>{pokemon.stats[5].stat.name} {pokemon.stats[5].base_stat}</p>
+                </>
+            )}
         </>
     )
 }
