8430f6a69ed12ffc00298070b9b371a85e686f82
Renan Martins <renanmartins1813@gmail.com>
2022-08-14 23:56:31 -0500

    refactor: pk-list-url, now the parameter is an object with offset and limit numbers

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const PK_LIST_URL = (current) => `https://pokeapi.co/api/v2/pokemon?offset=${current}&limit=15`;
+export const PK_LIST_URL = ({offset, limit}) => `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 export const SINGLE_PK_URL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
