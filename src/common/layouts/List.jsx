4b68b2ce7a36cbd5bb4fb592404041a858de6bb6
Renan Martins <renanmartins1813@gmail.com>
2022-08-15 21:46:04 -0500

    style: pokedex list and list-item css

--- src/common/layouts/List.jsx
+++ src/common/layouts/List.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function List({children}) {
     return (
-        <ul>
+        <ul className='pokedex__list'>
             {children}
         </ul>
         )
