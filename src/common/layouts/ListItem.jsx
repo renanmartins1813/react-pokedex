57e903371965ee6ca48adca8f61306265522fb64
Renan Martins <renanmartins1813@gmail.com>
2022-08-15 19:28:16 -0500

    feat: ListItem now show the pokemon id

--- src/common/layouts/ListItem.jsx
+++ src/common/layouts/ListItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import handleName from '../../utils/handleName'
+import getIdByURL from '../../utils/getIdByURL'
+import handleID from '../../utils/handleID'
 
-export default function ListItem({name, handleClick}) {
+export default function ListItem({name, url, handleClick}) {
     return (
         <li onClick={()=>handleClick(name)}>
-            {handleName(name)}
+            {handleID(getIdByURL(url))} {handleName(name)}
         </li>
     )
 }
